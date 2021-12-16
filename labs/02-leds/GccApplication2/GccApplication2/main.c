d0dd5bd4ce9f1c175e0bf842824d2c8b1dd2ebb7
xhroch01 <78789472+xhroch01@users.noreply.github.com>
2021-12-16 13:43:46 +0100

    xhroch01

---
+++ labs/02-leds/GccApplication2/GccApplication2/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 15.12.2021 10:32:06
+ * Author : pc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
